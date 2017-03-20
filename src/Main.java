2570124a159efb576fcb3dc5b986c6c68438bc94
Red-Code <18095953699@163.com>
2017-03-20 21:03:41 +0800

    complete heapSort

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,33 @@ public class Main {
         //直接插入排序
         int[] param_straightInsertionSort= {6,2,8,5,324,23423,56,2,87,3,42,436};
         sorting_util.straightInsertionSort(param_straightInsertionSort);
-        printResult("直接插入排序：",param_straightInsertionSort);
+        printResult("插入排序-直接插入排序：",param_straightInsertionSort);
 
         //希尔排序
         int[] param_shellSort= {6,2,8,5,324,23423,56,2,87,3,42,436};
         int incrementNum = param_shellSort.length/2;//增量
         sorting_util.shellSort(param_shellSort,incrementNum);
-        printResult("希尔排序：",param_shellSort);
+        printResult("插入排序-希尔排序：",param_shellSort);
 
         //冒泡排序
         int[] param_bubbleSort= {6,2,8,5,324,23423,56,2,87,3,42,436};
         sorting_util.bubbleSort(param_bubbleSort);
-        printResult("冒泡排序：",param_bubbleSort);
+        printResult("交换排序-冒泡排序：",param_bubbleSort);
 
         //快速排序
         int[] param_quickSort= {6,2,8,5,324,23423,56,2,87,3,42,436};
         sorting_util.quickSort(param_quickSort,0,param_quickSort.length-1);
-        printResult("快速排序：",param_quickSort);
+        printResult("交换排序-快速排序：",param_quickSort);
 
         //直接选择排序
         int[] param_selectionSort= {6,2,8,5,324,23423,56,2,87,3,42,436};
         sorting_util.selectionSort(param_selectionSort);
-        printResult("直接选择排序：",param_selectionSort);
+        printResult("选择排序-直接选择排序：",param_selectionSort);
+
+        //堆排序
+        int[] param_heapSort= {6,2,8,5,324,23423,56,2,87,3,42,436};
+        sorting_util.heapSort(param_heapSort);
+        printResult("选择排序-堆排序：",param_heapSort);
     }
 
     /**
